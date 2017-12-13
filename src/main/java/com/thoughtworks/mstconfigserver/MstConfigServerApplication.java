ae85a166fb014fadbd214dd3769731af0dd4b12a
YuanShenjian <sjyuan@thoughtworks.com>
2017-12-13 17:12:09 +0800

    Config repo

--- src/main/java/com/thoughtworks/mstconfigserver/MstConfigServerApplication.java
+++ src/main/java/com/thoughtworks/mstconfigserver/MstConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.thoughtworks.mstconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class MstConfigServerApplication {
 
 	public static void main(String[] args) {
