29e18d0c76e0b952da48c116b3f90a1a9f29d560
YuanShenjian <sjyuan@thoughtworks.com>
2017-12-13 14:13:30 +0800

    Init project

---
+++ src/main/java/com/thoughtworks/mstconfigserver/MstConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.mstconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MstConfigServerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thoughtworks/mstconfigserver/MstConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.mstconfigserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MstConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
